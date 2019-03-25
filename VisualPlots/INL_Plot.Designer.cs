deda3f48e67f28bc09a27102e64c5e24febf5024
Josh Guerrero <jgsplat@gmail.com>
2019-03-25 08:14:17 -0700

    Initial commit

---
+++ VisualPlots/INL_Plot.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace VisualPlots
+{
+    partial class INL_Plot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(INL_Plot));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnTrim = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.MyPrintDocument = new System.Drawing.Printing.PrintDocument();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.plot_INL = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.plot_INL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnTrim
+            // 
+            this.btnTrim.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTrim.Location = new System.Drawing.Point(1137, 30);
+            this.btnTrim.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTrim.Name = "btnTrim";
+            this.btnTrim.Size = new System.Drawing.Size(147, 50);
+            this.btnTrim.TabIndex = 2;
+            this.btnTrim.Text = "Trim Fuse Regs.";
+            this.btnTrim.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1307, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.printToolStripMenuItem.Text = "&Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, 31);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.plot_INL);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.btnTrim);
+            this.splitContainer1.Size = new System.Drawing.Size(1298, 577);
+            this.splitContainer1.SplitterDistance = 473;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // plot_INL
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.plot_INL.ChartAreas.Add(chartArea1);
+            this.plot_INL.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.plot_INL.Legends.Add(legend1);
+            this.plot_INL.Location = new System.Drawing.Point(0, 0);
+            this.plot_INL.Margin = new System.Windows.Forms.Padding(4);
+            this.plot_INL.Name = "plot_INL";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "INL";
+            this.plot_INL.Series.Add(series1);
+            this.plot_INL.Size = new System.Drawing.Size(1298, 473);
+            this.plot_INL.TabIndex = 1;
+            this.plot_INL.Text = "chart1";
+            // 
+            // INL_Plot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1307, 621);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "INL_Plot";
+            this.Text = "INL Plot";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.plot_INL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTrim;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument MyPrintDocument;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart plot_INL;
+    }
+}
+
---
+++ VisualPlots/INL_Plot.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization;       // Needed for chart
+
+namespace VisualPlots
+{
+    
+
+    public partial class INL_Plot : Form
+    {
+        List<int> codes = new List<int>();
+        List<double> inls = new List<double>();
+
+        public INL_Plot(bool trim_btn, List<int> codes, List<double> inls)
+        {
+            InitializeComponent();
+            this.codes = codes;
+            this.inls = inls;
+
+            btnTrim.Visible = trim_btn;
+
+        }
+
+        private void INL_Plot_Load(object sender, EventArgs e)
+        {
+            BubbleSort(ref codes, ref inls);
+            plot_INL.Series[0].Points.Clear();
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                plot_INL.Series[0].Points.AddXY(codes[i], inls[i]);
+            }
+            plot_INL.Series[0].Points.AddXY(32768, 0);
+        }
+
+        public void BubbleSort(ref List<short> codes, ref List<double> inls)
+        {
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                for (int j = i; j < codes.Count; j++)
+                {
+                    if (codes[j] < codes[i])
+                    {
+                        short temp_codes = codes[i];
+                        double temp_inls = inls[i];
+
+                        codes[i] = codes[j];
+                        inls[i] = inls[j];
+
+                        codes[j] = temp_codes;
+                        inls[j] = temp_inls;
+                    }
+                }
+            }
+
+            return;
+        }
+        public void BubbleSort(ref List<int> codes, ref List<double> inls)
+        {
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                for (int j = i; j < codes.Count; j++)
+                {
+                    if (codes[j] < codes[i])
+                    {
+                        int temp_codes = codes[i];
+                        double temp_inls = inls[i];
+
+                        codes[i] = codes[j];
+                        inls[i] = inls[j];
+
+                        codes[j] = temp_codes;
+                        inls[j] = temp_inls;
+                    }
+                }
+            }
+
+            return;
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var pd = new System.Drawing.Printing.PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(PrintChart);
+            pd.DefaultPageSettings.Landscape = true;
+
+            PrintDialog pdi = new PrintDialog();
+            pdi.Document = pd;
+            if (pdi.ShowDialog() == DialogResult.OK)
+            {
+                pdi.Document.Print();
+            }
+
+        }
+
+        void PrintChart(object sender, PrintPageEventArgs ev)
+        {
+            using (var f = new System.Drawing.Font("Arial", 10))
+            {
+                var size = ev.Graphics.MeasureString(Text, f);
+                ev.Graphics.DrawString("INL Plot", f, Brushes.Black, ev.PageBounds.X + (ev.PageBounds.Width - size.Width) / 2, ev.PageBounds.Y);
+            }
+
+            //Note, the chart printing code wants to print in pixels.
+            Rectangle marginBounds = ev.MarginBounds;
+            if (ev.Graphics.PageUnit != GraphicsUnit.Pixel)
+            {
+                ev.Graphics.PageUnit = GraphicsUnit.Pixel;
+                marginBounds.X = (int)(marginBounds.X * (ev.Graphics.DpiX / 100f));
+                marginBounds.Y = (int)(marginBounds.Y * (ev.Graphics.DpiY / 100f));
+                marginBounds.Width = (int)(marginBounds.Width * (ev.Graphics.DpiX / 100f));
+                marginBounds.Height = (int)(marginBounds.Height * (ev.Graphics.DpiY / 100f));
+            }
+
+            plot_INL.Printing.PrintPaint(ev.Graphics, marginBounds);
+        }
+
+        private void btnTrim_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+
+    }
+}
